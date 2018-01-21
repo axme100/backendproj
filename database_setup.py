66d973022e0a0b5f4aed9f272f6844804dc2f610
Max Carey <maxcarey@protonmail.com>
2018-01-21 12:32:34 -0600

    Make modifications to database_setup.py to get username and password functionality for API requests

--- database_setup.py
+++ database_setup.py
@@ -4,16 +4,27 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
-    email = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
     picture = Column(String(250))
+
+    # Takes in a plain password as an argument and stores a hash of it in the table
+    def hash_password(self, password):
+    	self.password_hash = pwd_context.encrypt(password)
+
+    # Takes a password in as an argument and return true if it's correct.
+    def verify_password(self, password):
+    	return pwd_context.verify(password, self.password.hash)
 
 
 class Story(Base):
