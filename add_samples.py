2f5ae823638c84ed24c58dc02498467a6c0084d6
Max Carey <maxcarey@protonmail.com>
2018-01-21 11:31:56 -0600

    Configure project.py to pep8 guidelines

--- add_samples.py
+++ add_samples.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
  
-from database_setup import Story, Base, Word
+from database_setup import Story, Base, Word, User
  
 engine = create_engine('sqlite:///storyandword.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -19,8 +19,14 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# Add example user
+user1 = User(name = "Wizard", email = "databasewizard@protonmail.ch", )
+session.add(user1)
+session.commit()
+
+
 #Add first example story
-story1 = Story(title = "Ejemplo 1", description = "Esta es una historia muy interesante", text = "Una vez fui al {} para buscarme un buen de {}")
+story1 = Story(title = "Ejemplo 1", description = "Esta es una historia muy interesante", text = "Una vez fui al {} para buscarme un buen de {}", user_id = user1.id)
 
 session.add(story1)
 session.commit()
@@ -38,7 +44,7 @@ session.commit()
 
 
 #Add second example story
-story2 = Story(title = "Example 2", description = "Esta es una historia muy aburrida", text = "Cuantos {} puedes comer. Yo solo puedo comer {}")
+story2 = Story(title = "Example 2", description = "Esta es una historia muy aburrida", text = "Cuantos {} puedes comer. Yo solo puedo comer {}", user_id = user1.id)
 
 session.add(story2)
 session.commit()
@@ -56,5 +62,29 @@ session.add(word4)
 session.commit()
 
 
-print("Added 2 example stories!")
+# Add a third story
+
+#Add second example story
+story3 = Story(title = "Example 3", description = "This is a sample story in English", text = "I like to eat {} because it is very delicous", user_id = user1.id)
+
+session.add(story3)
+session.commit()
+
+
+word5 = Word(word = "pizza", lexical_category = "msn", story = story3)
+
+session.add(word5)
+session.commit()
+
+
+word6 = Word(word = "delicious", lexical_category = "number", story = story3)
+
+session.add(word6)
+session.commit()
+
+
+
+
+
+print("Added 3 example stories!")
 
--- project.py
+++ project.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, make_response
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    jsonify,
+    url_for,
+    flash,
+    make_response
+)
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Story, Word, User
 from flask import session as login_session
-import random, string
+import random
+import string
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
 import json
 import requests
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 CLIENT_ID = json.loads(
   open('client_secrets.json', 'r').read())['web']['client_id']
 
-#Connect to Database and create database session
+# Connect to Database and create database session
 engine = create_engine('sqlite:///storyandword.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -21,8 +33,19 @@ session = DBSession()
 app = Flask(__name__)
 
 
-# Helper functions to implement user system
+@app.route('/stories/JSON')
+def storiesJSON():
+    stories = session.query(Story).all()
+    return jsonify(story=[i.serialize for i in stories])
 
+
+@app.route('/words/JSON')
+def wordsJSON():
+    words = session.query(Word).all()
+    return jsonify(Word=[i.serialize for i in words])
+
+
+# Helper functions to implement user system
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
@@ -43,6 +66,7 @@ def getUserID(email):
         return user.id
     except:
         return None
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -98,7 +122,8 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
+        response = make_response(json.dumps(
+            'Current user is already connected.'),
                                  200)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -129,23 +154,28 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;'
+    output += 'border-radius: 150px;-webkit-border-radius: 150px;'
+    output += '-moz-border-radius: 150px;">'
     flash("you are now logged in as %s" % login_session['username'])
     print("done!")
     return output
+
 
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
         print('Access Token is None')
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(json.dumps(
+            'Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     print('In gdisconnect access token is %s', access_token)
     print('User name is: ')
     print(login_session['username'])
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    url = 'https://accounts.google.com'
+    '/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print('result is ')
@@ -160,39 +190,50 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response = make_response(json.dumps(
+            'Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
     return response
 
 
 @app.route('/login')
 def showLogin():
-  state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
-  login_session['state'] = state
-  return render_template('login.html', STATE=state)
-  #return "The current session state is %s" % login_session['state']
+    state = ''.join(random.choice(
+        string.ascii_uppercase + string.digits) for x in range(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
 
 
 @app.route('/')
 def showHomePage():
     return render_template('homePage.html')
 
+
 @app.route('/createstory', methods=['GET', 'POST'])
 def createStory():
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
-      newStory = Story(title = request.form['title'], description = request.form['description'], text = request.form['text'], user_id = login_session['user_id'])
-      session.add(newStory)
-      session.commit()
-      numberOfBlanks = newStory.text.count('{')
-      return render_template('addWords.html', newStory = newStory, number_of_blanks = numberOfBlanks)
+        newStory = Story(
+            title=request.form['title'],
+            description=request.form['description'],
+            text=request.form['text'],
+            user_id=login_session['user_id'])
+        session.add(newStory)
+        session.commit()
+        numberOfBlanks = newStory.text.count('{')
+        return render_template(
+            'addWords.html',
+            newStory=newStory,
+            number_of_blanks=numberOfBlanks)
     # If it is a get request render the template with the form
     else:
         return render_template('createStory.html')
 
 
-@app.route('/addwords/<int:story_id>/<int:number_of_blanks>', methods=['GET', 'POST'])
+@app.route(
+    '/addwords/<int:story_id>/<int:number_of_blanks>',
+    methods=['GET', 'POST'])
 def addWords(story_id, number_of_blanks):
     if request.method == 'POST':
         data = request.form
@@ -200,26 +241,29 @@ def addWords(story_id, number_of_blanks):
         # At this point I have all the data in a nice dictionary form
         # I just need to loop through it pull out the corresponding word
         # and lexical_category values, and add them to the database.
-        
+
         for item in range(number_of_blanks):
-            newWord = Word(word = dataDict["word" + str(item)], lexical_category = dataDict["lexical_category" + str(item)], story_id = story_id, user_id = login_session['user_id'])
+            newWord = Word(
+                word=dataDict["word" + str(item)],
+                lexical_category=dataDict["lexical_category" + str(item)],
+                story_id=story_id, user_id=login_session['user_id'])
             session.add(newWord)
             session.commit()
-               
+
     return redirect(url_for('showStories'))
 
 
 @app.route('/stories')
 def showStories():
     stories = session.query(Story).all()
-    return render_template('stories.html', stories = stories)
+    return render_template('stories.html', stories=stories)
 
 
 @app.route('/viewstory/<int:story_id>')
 def viewStory(story_id):
     # Get the specific story that the user clicked on from the database
     story = session.query(Story).filter_by(id=story_id).one()
-    user = session.query(User).filter_by(id = story.user_id).one()
+    user = session.query(User).filter_by(id=story.user_id).one()
 
     # Get the specific words related to this story that the user clicked on
     words = session.query(Word).filter_by(story_id=story_id).all()
@@ -227,34 +271,37 @@ def viewStory(story_id):
     stringStory = story.text
     listOfWords = []
 
-    # Loop through the words all of the database rows returned and add each word to a list
+    # Loop through the words and add each word to a list
     for word in words:
-    	listOfWords.append(word.word)
+        listOfWords.append(word.word)
 
     # Change the listOfWords into a tuple so that I can pass it into .format()
     tupleList = tuple(listOfWords)
-    
-    # Create the complete story using .format() the * unpacks the tuple to prevent index out of range error
+
+    # Create the complete story using .format()
+    # The * unpacks the tuple to prevent index out of range error
     exampleStory = stringStory.format(*tupleList)
-    
+
     creator = getUserInfo(story.user_id)
-    
-    # Although I'm not sue if this is going tow work because there is not necessarily any login session
-    # that has been initiated, as the instructor does on the video.
-    # It looks like it properly renders the correct template.
-    if 'username' not in login_session or creator.id != login_session['user_id']:
-        return render_template('publicStory.html', story = story, exampleStory = exampleStory, user_email = user.email)
+
+    # Sometimes getting key errors here-look into login_session_object
+    if ('username' not in login_session or
+            creator.id != login_session['user_id']):
+        # Conditional satisfied
+        return render_template('publicStory.html',
+                               story=story,
+                               exampleStory=exampleStory,
+                               user_email=user.email)
     else:
-        return render_template('story.html', story = story, exampleStory = exampleStory, user_email = user.email)
+        return render_template('story.html',
+                               story=story,
+                               exampleStory=exampleStory,
+                               user_email=user.email)
+
 
 @app.route('/editstory/<int:story_id>', methods=['GET', 'POST'])
 def editStory(story_id):
-    # I think the best thing to do for this would be to pull out the story and each of the words.
-    # Onto a single page. You can click on each word to either edit or delete and edit and delete the story as you see fit.
-    # There will just have to be some warning that either (in the best of cases or later)
-    # doesn't let you submit the forms until the number of blanks matches the number of wordor, or,
-    # just tells the user (maybe for now), that they have to have the same amount of blanks as words (in the same order) if they don't want an error.
-    storyToEdit = session.query(Story).filter_by(id = story_id).one()
+    storyToEdit = session.query(Story).filter_by(id=story_id).one()
     if request.method == 'POST':
         storyToEdit.text = request.form['text']
         storyToEdit.title = request.form['title']
@@ -262,24 +309,29 @@ def editStory(story_id):
         session.add(storyToEdit)
         session.commit()
         numberOfBlanks = storyToEdit.text.count('{')
-        return redirect(url_for('editWords', number_of_blanks = numberOfBlanks, story_id = story_id))
+        return redirect(url_for('editWords',
+                                number_of_blanks=numberOfBlanks,
+                                story_id=story_id))
 
-   
-    return render_template('editStory.html', storyToEdit = storyToEdit)
+    return render_template('editStory.html', storyToEdit=storyToEdit)
 
-@app.route('/editwords/<int:story_id>/<int:number_of_blanks>', methods=['GET', 'POST'])
+
+@app.route('/editwords/<int:story_id>/<int:number_of_blanks>',
+           methods=['GET', 'POST'])
 def editWords(story_id, number_of_blanks):
-    
-    editedStory = session.query(Story).filter_by(id = story_id).one()
-    wordsToEdit = session.query(Word).filter_by(story_id = story_id).all()
-    
+
+    editedStory = session.query(Story).filter_by(id=story_id).one()
+    wordsToEdit = session.query(Word).filter_by(story_id=story_id).all()
+
     if 'username' not in login_session:
         return redirect('/login')
     if editedStory.user_id != login_session['user_id']:
-        return "<script>function myFunction() {alert('You are not authorized to edit the words of this story.');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction() {alert('You are not authorized"
+        "to edit the words of this story.');}"
+        "</script><body onload='myFunction()''>"
 
     if request.method == 'POST':
-        
+
         # This is the same code from the delete words function
         for word in wordsToEdit:
             session.delete(word)
@@ -288,33 +340,41 @@ def editWords(story_id, number_of_blanks):
         # This is the same code from the add words function
         data = request.form
         dataDict = data.to_dict()
-        
+
         for item in range(number_of_blanks):
-            newWord = Word(word = dataDict["word" + str(item)], lexical_category = dataDict["lexical_category" + str(item)], story_id = story_id)
+            newWord = Word(word=dataDict["word" + str(item)],
+                           lexical_category=dataDict["lexical"
+                           "_category" + str(item)],
+                           story_id=story_id)
             session.add(newWord)
-            session.commit()     
+            session.commit()
         return redirect(url_for('showStories'))
-    
-    return render_template('editWords.html', editedStory = editedStory, old_words = wordsToEdit, number_of_blanks = number_of_blanks)
+
+    return render_template('editWords.html',
+                           editedStory=editedStory,
+                           old_words=wordsToEdit,
+                           number_of_blanks=number_of_blanks)
 
 
 @app.route('/deletestory/<int:story_id>', methods=['GET', 'POST'])
 def deleteStory(story_id):
-    deletedWords = session.query(Word).filter_by(id = story_id).all()
-    deletedStory = session.query(Story).filter_by(id = story_id).one()
+    deletedWords = session.query(Word).filter_by(id=story_id).all()
+    deletedStory = session.query(Story).filter_by(id=story_id).one()
     if 'username' not in login_session:
         return redirect('/login')
     if deletedStory.user_id != login_session['user_id']:
-        return "<script>function myFunction() {alert('You are not authorized to delete this restaurant. Please create your own restaurant in order to delete.');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction() {alert('You are not authorized"
+        " to delete this restaurant. Please create your own restaurant"
+        " in order to delete.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         session.delete(deletedStory)
         for word in deletedWords:
             session.delete(word)
         session.commit()
         return redirect(url_for('showStories'))
-    return render_template('deleteStory.html', story_id = story_id)
+    return render_template('deleteStory.html', story_id=story_id)
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8000)
+    app.run(host='0.0.0.0', port=8000)
