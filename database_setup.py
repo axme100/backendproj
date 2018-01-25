91e0ac5a0ded749fd5bff873505423b084f4d1de
Max Carey <maxcarey@protonmail.com>
2018-01-25 11:47:20 -0600

    Format database_setup.py to pep8

--- database_setup.py
+++ database_setup.py
@@ -8,6 +8,7 @@ from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -18,71 +19,72 @@ class User(Base):
     password_hash = Column(String(64))
     picture = Column(String(250))
 
-    # Takes in a plain password as an argument and stores a hash of it in the table
+    # Takes in plain password as an argument and stores hash
     def hash_password(self, password):
-    	self.password_hash = pwd_context.encrypt(password)
+        self.password_hash = pwd_context.encrypt(password)
 
     # Takes a password in as an argument and return true if it's correct.
     def verify_password(self, password):
-    	return pwd_context.verify(password, self.password_hash)
+        return pwd_context.verify(password, self.password_hash)
 
 
 class Story(Base):
-	__tablename__ = 'story'
-	
-	title = Column(
-		String(80), nullable = False)
+    __tablename__ = 'story'
 
-	description = Column(
-		String(80), nullable = False)
+    title = Column(
+        String(80), nullable=False)
 
-	text = Column(
-		String(80), nullable = False)
+    description = Column(
+        String(80), nullable=False)
 
-	id = Column(
-		Integer, primary_key = True)
+    text = Column(
+        String(80), nullable=False)
 
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    id = Column(
+        Integer, primary_key=True)
 
-	@property
-	def serialize(self):
-		# Returns object data in easily serializeable format
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-		return {
-			'title' : self.title,
-			'description' : self.description,
-			'text' : self.text,
-			'id' : self.id
-		}
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
+
+        return {
+            'title': self.title,
+            'description': self.description,
+            'text': self.text,
+            'id': self.id
+        }
+
 
 class Word(Base):
-	__tablename__ = 'word'
+    __tablename__ = 'word'
 
-	word = Column(String(80), nullable = False)
-	lexical_category = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
-	order = Column(Integer, nullable = False)
-	story_id = Column(
-		Integer, ForeignKey('story.id'))
-	story = relationship(Story)
+    word = Column(String(80), nullable=False)
+    lexical_category = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    order = Column(Integer, nullable=False)
+    story_id = Column(
+        Integer, ForeignKey('story.id'))
+    story = relationship(Story)
 
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-		# Returns object data in easily serializeable format
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
 
-		return {
-			'word' : self.word,
-			'lexical_category' : self.lexical_category,
-			'id' : self.id,
-			'story_id' : self.story_id,
-			'user_id' : self.user_id
-		}
+        return {
+            'word': self.word,
+            'lexical_category': self.lexical_category,
+            'id': self.id,
+            'story_id': self.story_id,
+            'user_id': self.user_id
+        }
 
 engine = create_engine(
-'sqlite:///storyandword.db')
+                      'sqlite:///storyandword.db')
 
 Base.metadata.create_all(engine)
